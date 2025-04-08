63c9d1ddecef8252bf90e140a7ad7800ba6e436b
stefanpython <stefanhtml@yahoo.com>
2025-04-08 14:31:47 +0300

    Define passport.js

---
+++ server/config/passport.js
@@ -0,0 +1,69 @@
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+
+module.exports = (passport) => {
+  // Local Strategy
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "email" },
+      async (email, password, done) => {
+        try {
+          // Match user
+          const user = await User.findOne({ email });
+          if (!user) {
+            return done(null, false, {
+              message: "That email is not registered",
+            });
+          }
+
+          // Match password
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Password incorrect" });
+          }
+        } catch (err) {
+          return done(err);
+        }
+      }
+    )
+  );
+
+  // JWT Strategy
+  const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  };
+
+  passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      try {
+        const user = await User.findById(jwt_payload.id);
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, false);
+      } catch (err) {
+        return done(err, false);
+      }
+    })
+  );
+
+  // Serialize and Deserialize User
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  });
+};
